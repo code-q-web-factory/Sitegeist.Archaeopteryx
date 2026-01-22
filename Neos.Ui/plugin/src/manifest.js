36402ada2a996ece8fea062093d2e61f07106373
Roland Schütz <mail@rolandschuetz.at>
2026-01-22 14:46:37 +0100

    FEAT: Expose Field from 'react-final-form' for extensibility

--- Neos.Ui/plugin/src/manifest.js
+++ Neos.Ui/plugin/src/manifest.js
@@ -4,6 +4,12 @@ import {registerLinkTypes, registerDialog, createEditor} from '@sitegeist/archae
 import {registerInspectorEditors} from '@sitegeist/archaeopteryx-inspector-editor';
 import {registerLinkButton} from '@sitegeist/archaeopteryx-link-button';
 
+// Minimal change to Archaeopteryx manifest.js
+// Expose Archaeopteryx's Field wrapper for external link type implementations
+// This Field is wrapped with FieldGroupContext to properly prefix field names
+import {Field} from '@sitegeist/archaeopteryx-core/src/framework';
+window['@sitegeist/archaeopteryx'] = { Field };
+
 manifest('@sitegeist/archaeopteryx-plugin', {}, (globalRegistry, {store, configuration, routes}) => {
     const editor = createEditor();
     const neosContextProperties = {globalRegistry, store, configuration, routes};
